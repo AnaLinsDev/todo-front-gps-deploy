36063bcb99f76c3ddacc5e064e2b870d30295494
AnaLinsDev <analinsdev@gmail.com>
2021-07-19 17:27:12 -0300

    primeiro commit

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './home.scss'
+
+const HomePage = () => (
+    <div>
+        <h1>HOME !!!</h1>
+    </div>
+);
+
+export default HomePage
---
+++ src/pages/todo-list/todo-list.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './todo-list.scss'
+
+const TodoListPage = () => (
+    <div>
+        <h1>TODO LIST  !!!</h1>
+    </div>
+);
+
+export default TodoListPage
